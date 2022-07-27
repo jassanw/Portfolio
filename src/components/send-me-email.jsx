b24f89b6b5a907ebb246a1f35d4bf7e982e52c87
Jassan <jlwirth@outlook.com>
2022-07-27 16:49:31 -0700

    background image

--- src/components/send-me-email.jsx
+++ src/components/send-me-email.jsx
@@ -1,11 +1,11 @@
 const SendMeEmail = () => {
     return ( 
         <a href="mailto:jlwirth@outlook.com"> 
-            <div class="p-6 max-w-xs bg-white rounded-xl shadow-lg flex items-center space-x-4">
-                <div class="shrink-0">
-                    <img class="h-12 w-12" src="/sendmail.png" alt="ChitChat Logo"/>
+            <div className="p-6 max-w-xs bg-white rounded-xl shadow-lg flex items-center space-x-4">
+                <div className="shrink-0">
+                    <img className="h-12 w-12" src="/sendmail.png" alt="ChitChat Logo"/>
                 </div>
-                <p class="text-xl font-medium text-black">Send me an email</p>
+                <p className="text-xl font-medium text-black">Send me an email</p>
             </div>
         </a>
     )
